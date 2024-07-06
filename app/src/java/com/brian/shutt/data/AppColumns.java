17daa7ce7e0d4caa7de2cc1f86158e98c7569a28
bryangikunga <bryangikunga@gmail.com>
2024-07-06 15:01:07 +0300

    AppColumns.java

---
+++ app/src/java/com/brian/shutt/data/AppColumns.java
@@ -0,0 +1,33 @@
+com.addie.timesapp.data;
+
+import net.simonvt.schematic.annotation.AutoIncrement;
+import net.simonvt.schematic.annotation.DataType;
+import net.simonvt.schematic.annotation.PrimaryKey;
+import net.simonvt.schematic.annotation.Unique;
+
+import static net.simonvt.schematic.annotation.DataType.Type.INTEGER;
+import static net.simonvt.schematic.annotation.DataType.Type.TEXT;
+
+/**
+ * Defines the columns needed in the database for storing app information
+ */
+public interface AppColumns {
+
+    @DataType(INTEGER) @PrimaryKey
+    @AutoIncrement
+    String _ID = "_id";
+
+    @DataType(TEXT)
+    String APP_TITLE="app_title";
+
+    @DataType(TEXT) @Unique
+    String PACKAGE_NAME="package_name";
+
+    @DataType(INTEGER)
+    String PALETTE_COLOR ="palette_color";
+
+    @DataType(INTEGER)
+    String TEXT_COLOR = "text_color";
+
+
+}
